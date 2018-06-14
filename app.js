567c01cd91821c3594379b343958450ec4f29f53
Unknown <spdm1254@gmail.com>
2018-06-14 11:20:35 +0900

    cors 기능 추가

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
