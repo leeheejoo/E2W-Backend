7b0b7f6a329456557c1ae082bf0c52d54df913a8
Unknown <spdm1254@gmail.com>
2018-06-19 14:43:22 +0900

    mongo db reconnect 기능 추가

--- db/mongo.js
+++ db/mongo.js
@@ -16,25 +16,25 @@ db.on('reconnected', function () {
     console.log('MongoDB reconnected!');
 });
 
-db.on('disconnected', function() {
+db.on('disconnected', async function() {
     console.log('MongoDB disconnected!');
     
     try {
-        mongoose.connect(config.mongoUrl, {auto_reconnect:true, reconnectTries: Number.MAX_VALUE });
+        await mongoose.connect(config.mongoUrl, {auto_reconnect:true, reconnectTries: Number.MAX_VALUE });
     }
     catch(e){
-        console.log(e);
+        //console.log(e);
     }
     
 });
 
-try {
-    mongoose.connect(config.mongoUrl, {auto_reconnect:true, reconnectTries: Number.MAX_VALUE });
-}
-catch(e){
-    console.log(e);
-}
-
-
+(async () => {
+    try {
+        await mongoose.connect(config.mongoUrl, {auto_reconnect:true, reconnectTries: Number.MAX_VALUE });
+    }
+    catch(e){
+        //console.log(e);
+    }
+})();
 
 module.exports = mongoose;
